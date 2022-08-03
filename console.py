2ecd8c7e35a0b2291c352fb8a239bed2c13b880b
Rory Conlon <roryconlon1@gmail.com>
2022-08-03 10:35:02 +0100

    readme

--- console.py
+++ console.py
@@ -16,11 +16,11 @@ sport_repository.save(sport_2)
 sport_3 = Sport("Ice Hockey", True)
 sport_repository.save(sport_3)
 
-product_1 = Product("Football", "size 5 mitre", 7, 3, 10, "Ball", sport_1)
+product_1 = Product("Football", "Size-5, Mitre", 7, 5, 14, "Ball", sport_1)
 product_repository.save(product_1)
 
-product_2 = Product("Racket", "Wilson - Red", 5, 35, 50, "Equipment", sport_2)
+product_2 = Product("Racket", "Wilson Blade Pro - Red", 5, 35, 100, "Equipment", sport_2)
 product_repository.save(product_2)
 
-product_3 = Product("Puck", "Bauer - Black", 3, 1, 4, "Equipment", sport_3)
+product_3 = Product("Ice Skates", "Bauer Vapor Hyperlite - Black", 3, 500, 700, "Equipment", sport_3)
 product_repository.save(product_3)
